eabb3bc9d6da1e3af83ad8f45d94ec87715f8e71
dumbdemon <huynh.tan37@gmail.com>
2022-11-06 21:23:09 -0600

    Populated `/about`; Truncated/moved code
    Signed-off-by: dumbdemon <huynh.tan37@gmail.com>

--- src/main/java/com/terransky/StuffnThings/Commons.java
+++ src/main/java/com/terransky/StuffnThings/Commons.java
@@ -6,9 +6,8 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 
 import java.awt.*;
-import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.*;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class Commons {
@@ -17,22 +16,6 @@ public class Commons {
     public static final Dotenv config = Dotenv.configure().load();
     public static final boolean isTestingMode = config.get("TESTING_MODE").equals("true");
     public static final boolean enableDatabase = config.get("ENABLE_DATABASE").equals("true");
-    private static final NavigableMap<Float, String> suffixes = new TreeMap<>();
-
-    static {
-        suffixes.put(1_000f, "\sThousand");
-        suffixes.put(1_000_000f, "\sMillion");
-        suffixes.put(1_000_000_000f, "\sBillion");
-        suffixes.put(1_000_000_000_000f, "\sTrillion");
-        suffixes.put(1_000_000_000_000_000f, "\sQuadrillion");
-        suffixes.put(1_000_000_000_000_000_000f, "\sQuintillion");
-        suffixes.put(1_000_000_000_000_000_000_000f, "\sSextillion");
-        suffixes.put(1_000_000_000_000_000_000_000_000f, "\sSeptillion");
-        suffixes.put(1_000_000_000_000_000_000_000_000_000f, "\sOctillion");
-        suffixes.put(1_000_000_000_000_000_000_000_000_000_000f, "\sNonillion");
-        suffixes.put(1_000_000_000_000_000_000_000_000_000_000_000f, "\sDecillion");
-        suffixes.put(1_000_000_000_000_000_000_000_000_000_000_000_000f, "\sUndecillion");
-    }
 
     @Contract(pure = true)
     private Commons() {
@@ -56,27 +39,5 @@ public class Commons {
         //Moderation
 
         return permissionList;
-    }
-
-    /**
-     * Makes extra large numbers look nice~ <br />
-     * Code Courtesy of <a href="https://stackoverflow.com/questions/4753251/how-to-go-about-formatting-1200-to-1-2k-in-java">assylias's answer</a> on stackoverflow.
-     *
-     * @return {@link String} that contains a two-point decimal and the scale of value.
-     */
-    public static String largeNumberFormat(float value) {
-        DecimalFormat simpleNum = new DecimalFormat("#.##");
-
-        if (value == Float.MIN_VALUE) return largeNumberFormat(Float.MIN_VALUE + 1);
-        if (value < 0) return "-" + largeNumberFormat(-value);
-        if (value < 1000) return Float.toString(value);
-
-        Map.Entry<Float, String> e = suffixes.floorEntry(value);
-        float divideBy = e.getKey();
-        String suffix = e.getValue();
-
-        float truncated = value / (divideBy / 10);
-        boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
-        return hasDecimal ? simpleNum.format(truncated / 10d) + suffix : simpleNum.format(truncated / 10) + suffix;
     }
 }
--- src/main/java/com/terransky/StuffnThings/commandSystem/commands/mtg/calculateRats.java
+++ src/main/java/com/terransky/StuffnThings/commandSystem/commands/mtg/calculateRats.java
@@ -12,8 +12,50 @@ import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.jetbrains.annotations.NotNull;
 
 import java.text.DecimalFormat;
+import java.util.Map;
+import java.util.NavigableMap;
+import java.util.TreeMap;
 
 public class calculateRats implements ISlashCommand {
+    private static final NavigableMap<Float, String> suffixes = new TreeMap<>();
+
+    static {
+        suffixes.put(1e3f, "\sThousand");
+        suffixes.put(1e6f, "\sMillion");
+        suffixes.put(1e9f, "\sBillion");
+        suffixes.put(1e12f, "\sTrillion");
+        suffixes.put(1e15f, "\sQuadrillion");
+        suffixes.put(1e18f, "\sQuintillion");
+        suffixes.put(1e21f, "\sSextillion");
+        suffixes.put(1e24f, "\sSeptillion");
+        suffixes.put(1e27f, "\sOctillion");
+        suffixes.put(1e30f, "\sNonillion");
+        suffixes.put(1e33f, "\sDecillion");
+        suffixes.put(1e36f, "\sUndecillion");
+    }
+
+    /**
+     * Makes extra large numbers look nice~ <br />
+     * Code Courtesy of <a href="https://stackoverflow.com/questions/4753251/how-to-go-about-formatting-1200-to-1-2k-in-java">assylias's answer</a> on stackoverflow.
+     *
+     * @return {@link String} that contains a two-point decimal and the scale of value.
+     */
+    public static String largeNumberFormat(float value) {
+        DecimalFormat simpleNum = new DecimalFormat("#.##");
+
+        if (value == Float.MIN_VALUE) return largeNumberFormat(Float.MIN_VALUE + 1);
+        if (value < 0) return "-" + largeNumberFormat(-value);
+        if (value < 1000) return Float.toString(value);
+
+        Map.Entry<Float, String> e = suffixes.floorEntry(value);
+        float divideBy = e.getKey();
+        String suffix = e.getValue();
+
+        float truncated = value / (divideBy / 10);
+        boolean hasDecimal = truncated < 100 && (truncated / 10d) != (truncated / 10);
+        return hasDecimal ? simpleNum.format(truncated / 10d) + suffix : simpleNum.format(truncated / 10) + suffix;
+    }
+
     @Override
     public String getName() {
         return "ner";
@@ -24,9 +66,9 @@ public class calculateRats implements ISlashCommand {
         return Commands.slash(this.getName(), "How many rats you have?")
             .addOptions(
                 new OptionData(OptionType.INTEGER, "start-count", "How many do you have right now?", true)
-                    .setRequiredRange(3, Integer.MAX_VALUE),
+                    .setMinValue(3),
                 new OptionData(OptionType.INTEGER, "iterations", "How many iterations?", true)
-                    .setRequiredRange(1, Integer.MAX_VALUE)
+                    .setMinValue(1)
             );
     }
 
@@ -52,7 +94,7 @@ public class calculateRats implements ISlashCommand {
             eb.setDescription("Yes.")
                 .addField("Final Count", "INFINITE", false);
         } else eb.setDescription("No.")
-            .addField("Final Count (Short-Hand)", ("%s rats".formatted(Commons.largeNumberFormat(finalCNT)).replace(".0\s", "\s")), false)
+            .addField("Final Count (Short-Hand)", ("%s rats".formatted(largeNumberFormat(finalCNT)).replace(".0\s", "\s")), false)
             .addField("Final Count (Full Number)", "%s rats".formatted(largeNumber.format(finalCNT)), false);
 
         event.getHook().sendMessageEmbeds(eb.build()).queue();
