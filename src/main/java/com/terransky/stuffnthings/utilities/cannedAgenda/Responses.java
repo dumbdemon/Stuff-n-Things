8a2807b1fccf75c7d81523ee2b5e61bbf41aee44
dumbdemon <20498145+dumbdemon@users.noreply.github.com>
2022-12-26 15:34:56 -0600

    Added isAllCaps boolean to Responses.java
    Signed-off-by: dumbdemon <20498145+dumbdemon@users.noreply.github.com>

--- src/main/java/com/terransky/stuffnthings/utilities/cannedAgenda/Responses.java
+++ src/main/java/com/terransky/stuffnthings/utilities/cannedAgenda/Responses.java
@@ -6,18 +6,27 @@ import org.jetbrains.annotations.NotNull;
 
 public enum Responses {
 
-    //todo: Add Responses
     INTERACTION_FAILED("An error occurred whilst executing this interaction, please submit an issue [here](%s).".formatted(Config.getErrorReportingURL())),
     GUILD_ONLY("This interaction is guild only. Please use this interaction in a guild.");
 
     private final String message;
+    private final boolean isAllCaps;
 
     Responses(String message) {
+        this(message, false);
+    }
+
+    Responses(String message, boolean isAllCaps) {
         this.message = message;
+        this.isAllCaps = isAllCaps;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public boolean isAllCaps() {
+        return isAllCaps;
     }
 
     @NotNull
@@ -25,6 +34,6 @@ public enum Responses {
         if (interaction == InteractionType.UNKNOWN)
             throw new IllegalArgumentException("Interaction cannot be unknown.");
 
-        return getMessage().replaceAll("(?i)interaction", interaction.getName());
+        return getMessage().replaceAll("(?i)interaction", isAllCaps() ? interaction.getName().toUpperCase() : interaction.getName());
     }
 }
