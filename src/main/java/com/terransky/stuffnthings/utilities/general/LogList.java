5e6531bca719cbbe89bd806485f640cee96243e0
dumbdemon <20498145+dumbdemon@users.noreply.github.com>
2022-12-29 14:07:55 -0600

    Changed parameters from List<?> to Collection<?>
    Signed-off-by: dumbdemon <20498145+dumbdemon@users.noreply.github.com>

--- src/main/java/com/terransky/stuffnthings/utilities/general/LogList.java
+++ src/main/java/com/terransky/stuffnthings/utilities/general/LogList.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.List;
+import java.util.Collection;
 
 @SuppressWarnings("unused")
 public class LogList {
@@ -12,43 +12,43 @@ public class LogList {
     private LogList() {
     }
 
-    public static void info(@NotNull List<?> aList, Logger log) {
-        for (Object listItem : aList) {
-            log.info(listItem.toString());
+    public static void info(@NotNull Collection<?> objects, Logger log) {
+        for (Object obj : objects) {
+            log.info(obj.toString());
         }
     }
 
-    public static void info(@NotNull List<?> aList, Class<?> clazz) {
-        info(aList, LoggerFactory.getLogger(clazz));
+    public static void info(@NotNull Collection<?> objects, Class<?> clazz) {
+        info(objects, LoggerFactory.getLogger(clazz));
     }
 
-    public static void debug(@NotNull List<?> aList, Logger log) {
-        for (Object listItem : aList) {
-            log.debug(listItem.toString());
+    public static void debug(@NotNull Collection<?> objects, Logger log) {
+        for (Object obj : objects) {
+            log.debug(obj.toString());
         }
     }
 
-    public static void debug(@NotNull List<?> aList, Class<?> clazz) {
-        debug(aList, LoggerFactory.getLogger(clazz));
+    public static void debug(@NotNull Collection<?> objects, Class<?> clazz) {
+        debug(objects, LoggerFactory.getLogger(clazz));
     }
 
-    public static void error(@NotNull List<?> aList, Logger log) {
-        for (Object listItem : aList) {
-            log.error(listItem.toString());
+    public static void error(@NotNull Collection<?> objects, Logger log) {
+        for (Object obj : objects) {
+            log.error(obj.toString());
         }
     }
 
-    public static void error(@NotNull List<?> aList, Class<?> clazz) {
-        error(aList, LoggerFactory.getLogger(clazz));
+    public static void error(@NotNull Collection<?> objects, Class<?> clazz) {
+        error(objects, LoggerFactory.getLogger(clazz));
     }
 
-    public static void warn(@NotNull List<?> aList, Logger log) {
-        for (Object listItem : aList) {
-            log.warn(listItem.toString());
+    public static void warn(@NotNull Collection<?> objects, Logger log) {
+        for (Object obj : objects) {
+            log.warn(obj.toString());
         }
     }
 
-    public static void warn(@NotNull List<?> aList, Class<?> clazz) {
-        warn(aList, LoggerFactory.getLogger(clazz));
+    public static void warn(@NotNull Collection<?> objects, Class<?> clazz) {
+        warn(objects, LoggerFactory.getLogger(clazz));
     }
 }
