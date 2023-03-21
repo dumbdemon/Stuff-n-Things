8a79b40762aaa823dd175c7ca7f30c3b6ba87b53
dumbdemon <20498145+dumbdemon@users.noreply.github.com>
2023-03-20 19:40:33 -0500

    Changed Color param to RGB param for EmbedColor.java
    Signed-off-by: dumbdemon <20498145+dumbdemon@users.noreply.github.com>

--- src/main/java/com/terransky/stuffnthings/utilities/command/EmbedColor.java
+++ src/main/java/com/terransky/stuffnthings/utilities/command/EmbedColor.java
@@ -4,14 +4,14 @@ import java.awt.*;
 
 public enum EmbedColor {
 
-    DEFAULT(new Color(102, 51, 102)),
-    SUB_DEFAULT(new Color(153, 102, 153)),
-    ERROR(new Color(153, 0, 102));
+    DEFAULT(102, 51, 102),
+    SUB_DEFAULT(153, 102, 153),
+    ERROR(153, 0, 102);
 
     private final Color color;
 
-    EmbedColor(Color color) {
-        this.color = color;
+    EmbedColor(int r, int g, int b) {
+        this.color = new Color(r, g, b);
     }
 
     public Color getColor() {
